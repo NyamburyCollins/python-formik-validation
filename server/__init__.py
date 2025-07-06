fda02cc6d41180d816c95bb24943d3703c5c7588
*Collins <nyamburycollins@mail.com>
2025-07-06 21:02:54 +0300

    Completed

---
+++ server/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_migrate import Migrate
+from .models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    Migrate(app, db)
+
+    return app
